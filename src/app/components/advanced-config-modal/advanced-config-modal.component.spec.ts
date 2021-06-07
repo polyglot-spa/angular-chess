91387e3eb0f0f47dd8231774dff0fee678f48e88
cartereric <eric.eugene.carter@gmail.com>
2021-06-07 19:46:43 -0400

    Add tests to Angular specs

--- src/app/components/advanced-config-modal/advanced-config-modal.component.spec.ts
+++ src/app/components/advanced-config-modal/advanced-config-modal.component.spec.ts
@@ -1,14 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { AdvancedConfigModalComponent } from './advanced-config-modal.component';
+import { ModalService } from '../../Services/modal.service';
+import createSpy = jasmine.createSpy;
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ChessStarterService } from '../../Services/chess-starter.service';
 
 describe('AdvancedConfigModalComponent', () => {
   let component: AdvancedConfigModalComponent;
   let fixture: ComponentFixture<AdvancedConfigModalComponent>;
+  let mockModalService;
+  let mockNgbModal;
+  let mockChessStarterService;
 
   beforeEach(async () => {
+    mockModalService = {openAdvancedConfigModal: {subscribe: createSpy('openAdvancedConfigModal subscribe')}};
+    mockNgbModal = {dismissAll: createSpy('dismissAll')};
+    mockChessStarterService = {advanceConfigStartGame: {emit: createSpy('advanceConfigStartGame emit')}};
     await TestBed.configureTestingModule({
-      declarations: [ AdvancedConfigModalComponent ]
+      declarations: [ AdvancedConfigModalComponent ],
+      providers: [
+        { provide: ModalService, useValue: mockModalService },
+        { provide: NgbModal, useValue: mockNgbModal },
+        { provide: ChessStarterService, useValue: mockChessStarterService }
+      ]
     })
     .compileComponents();
   });
@@ -27,5 +41,25 @@ describe('AdvancedConfigModalComponent', () => {
     expect(component.fen).toBe('');
     expect(component.color).toBe('white');
     expect(component.selfPlay).toBe(false);
+  });
+
+  it('should subscribe to the openAdvancedConfigModal event when ngOnInit is called', () => {
+    component.ngOnInit();
+    expect(mockModalService.openAdvancedConfigModal.subscribe).toHaveBeenCalled();
+  });
+
+  it('should call NgbModal\'s dismissAll method when closing the Advanced Config Modal', () => {
+    component.closeAdvancedConfigModal();
+    expect(mockNgbModal.dismissAll).toHaveBeenCalled();
+  });
+  describe('advancedConfigStartGame method', () => {
+    it('should close the modal when called', () => {
+      component.advancedConfigStartGame();
+      expect(mockNgbModal.dismissAll).toHaveBeenCalled();
+    });
+    it('should have the ChessStarterService emit the data properly', () => {
+      component.advancedConfigStartGame();
+      expect(mockChessStarterService.advanceConfigStartGame.emit).toHaveBeenCalledWith({ fen: '', color: 'white', selfPlay: false});
+    });
   });
 });
--- src/app/components/angular-chess-header/angular-chess-header.component.spec.ts
+++ src/app/components/angular-chess-header/angular-chess-header.component.spec.ts
@@ -1,14 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AngularChessHeaderComponent } from './angular-chess-header.component';
+import {ModalService} from '../../Services/modal.service';
+import {ChessStarterService} from '../../Services/chess-starter.service';
+import createSpy = jasmine.createSpy;
 
 describe('AngularChessHeaderComponent', () => {
   let component: AngularChessHeaderComponent;
   let fixture: ComponentFixture<AngularChessHeaderComponent>;
+  let mockModalService;
+  let mockChessStarterService;
 
   beforeEach(async () => {
+    mockModalService = {openAdvancedConfigModal: {emit: createSpy('openAdvancedConfigModal emit')}};
+    mockChessStarterService = {quickStartGame: {emit: createSpy('quickStartGame emit')}};
     await TestBed.configureTestingModule({
-      declarations: [ AngularChessHeaderComponent ]
+      declarations: [ AngularChessHeaderComponent ],
+      providers: [
+        { provide: ModalService, useValue: mockModalService },
+        { provide: ChessStarterService, useValue: mockChessStarterService }
+      ]
     })
     .compileComponents();
   });
@@ -21,5 +32,15 @@ describe('AngularChessHeaderComponent', () => {
 
   it('should create the Angular Chess Header component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have the ChessStarter Service emit the quickStartGame event when emitQuickStartGame is called', () => {
+    component.emitQuickStartGame();
+    expect(mockChessStarterService.quickStartGame.emit).toHaveBeenCalled();
+  });
+
+  it('should have the Modal Service emit the openAdvancedConfigModal event when emitQuickStartGame is called', () => {
+    component.openAdvancedConfigModal();
+    expect(mockModalService.openAdvancedConfigModal.emit).toHaveBeenCalled();
   });
 });
