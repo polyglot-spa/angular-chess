f7a1e30e8ea8c5cfbefd9b2a1627a41b183bcac1
cartereric <eric.eugene.carter@gmail.com>
2021-07-13 19:03:49 -0400

    Add event emitter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-chess';
+  ngOnInit(): void {
+    let emitter;
+    let getWindowEmitterMaxTriers = null;
+    const waitForEmitterOnWindow = (resolve, reject) => {
+      if (!getWindowEmitterMaxTriers) {
+        getWindowEmitterMaxTriers = 10;
+      }
+      // @ts-ignore
+      if (!window.emitter) {
+        if (getWindowEmitterMaxTriers > 0) {
+          setTimeout(waitForEmitterOnWindow.bind(this, resolve, reject), 300);
+        }
+      } else {
+        resolve();
+      }
+    };
+    const attachEmitter = () => new Promise((resolve, reject) => {
+      waitForEmitterOnWindow(resolve, reject);
+    });
+    attachEmitter().then(() => {
+      // @ts-ignore
+      emitter = window.emitter;
+      emitter.emit('hello', 'Angular MFE');
+    });
+  }
 }
